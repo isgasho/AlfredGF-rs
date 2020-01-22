82d31d4ff45b5693bac6d18e99f78c97a7ffd0a5
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-01-21 17:53:17 -0800

    writing context things

--- src/alfredgf.rs
+++ src/alfredgf.rs
@@ -1,5 +1,14 @@
 use wgpu::{
-    //
+    PowerPreference,
+    Surface,
+    Device,
+    Queue,
+    BackendBit,
+    RequestAdapterOptions,
+    Adapter,
+    Limits,
+    DeviceDescriptor,
+    Extensions,
 };
 
 use winit::{
@@ -47,8 +56,8 @@ static mut CURRENT_WINDOW: Option<AFWindow> = None;
 
 impl AFWindow {
 
-    fn new<'a>(config: &AFWindowConfig) -> &'a Self {
-        unsafe {
+    pub fn new<'a>(config: &AFWindowConfig) -> &'a Self {
+        return unsafe {
             match &CURRENT_WINDOW {
                 None => {
                     let builder: WindowBuilder = WindowBuilder::new()
@@ -81,49 +90,66 @@ impl AFWindow {
                 Some(AFWindow) => {
                     // nothing... there's already a window
                 }
-            }
+            };
 
             CURRENT_WINDOW.as_ref().unwrap()
-        }
-//        unsafe {
-//            let mut w: Option<&AFWindow> = None;
-//            let k = match &CURRENT_WINDOW {
-//                None => {
-//                    let builder: WindowBuilder = WindowBuilder::new()
-//                        .with_window_icon(Icon::from_rgba(
-//                            config.icon.data.to_vec(),
-//                            config.icon.width,
-//                            config.icon.height,
-//                        ).ok())
-//                        .with_min_inner_size(
-//                            PhysicalSize::new(config.min_size[0], config.min_size[1]))
-//                        .with_max_inner_size(
-//                            PhysicalSize::new(config.max_size[0], config.max_size[1]))
-//                        .with_inner_size(
-//                            PhysicalSize::new(config.start_size[0], config.start_size[1])
-//                        )
-//                        .with_title(config.title)
-//                        .with_resizable(config.resizeable)
-//                        .with_always_on_top(config.always_on_top)
-//                        .with_maximized(config.maximized);
-//
-//                    let event_loop: EventLoop<()> = EventLoop::new();
-//                    let window = builder.build(&event_loop).unwrap();
-//                    let aw: &AFWindow = &AFWindow {
-//                        window,
-//                        event_loop,
-//                    };
-//                    w = Option::Some(aw);
-//
-//                    w.unwrap()
-//                }
-//                Some(&AFWindow) => {
-//                    w.unwrap()
-//                }
-//            };
-//
-//            return k;
-//        }
+        };
+    }
+
+}
+
+pub struct AFContextConfig {
+
+    pub anisotropic_filtering: bool,
+    pub power_preference: PowerPreference,
+
+}
+
+pub struct AFContext {
+
+    size: PhysicalSize<u32>,
+    surface: Surface,
+    device: Device,
+    queue: Queue,
+
+}
+
+static mut CURRENT_CONTEXT: Option<AFContext> = None;
+
+impl AFContext {
+
+    pub fn new<'a>(window: &AFWindow, config: &AFContextConfig) -> &'a Self {
+        return unsafe {
+            match &CURRENT_CONTEXT {
+                None => {
+                    let size: PhysicalSize<u32> = window.window.inner_size();
+                    let surface: Surface = Surface::create(&window.window);
+                    let adapter: Adapter = Adapter::request(&RequestAdapterOptions {
+                        power_preference: config.power_preference,
+                        backends: BackendBit::PRIMARY, // defaults to Vulkan / Metal
+                    }).unwrap();
+
+                    let (device, mut queue): (Device, Queue) = adapter.request_device(&DeviceDescriptor {
+                        extensions: Extensions {
+                            anisotropic_filtering: config.anisotropic_filtering,
+                        },
+                        limits: Limits::default(),
+                    });
+
+                    CURRENT_CONTEXT = Option::Some(AFContext {
+                        size,
+                        surface,
+                        device,
+                        queue,
+                    });
+                }
+                Some(AFContext) => {
+                    //
+                }
+            }
+
+            CURRENT_CONTEXT.as_ref().unwrap()
+        };
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 mod alfredgf;
 
+use wgpu::{
+    PowerPreference
+};
+
 fn main() {
-    //
+    let window_config: alfredgf::AFWindowConfig = alfredgf::AFWindowConfig {
+        icon: &alfredgf::AFImage {
+            data: &[],
+            width: 0,
+            height: 0,
+        },
+        min_size: [1280, 720],
+        max_size: [1280, 720],
+        start_size: [1280, 720],
+        title: "Test",
+        resizeable: true,
+        visible: true,
+        always_on_top: false,
+        maximized: false,
+    };
+    let window: &alfredgf::AFWindow = alfredgf::AFWindow::new(&window_config);
+
+    let context_config: alfredgf::AFContextConfig = alfredgf::AFContextConfig {
+        anisotropic_filtering: false,
+        power_preference: PowerPreference::Default,
+    };
+    let context: &alfredgf::AFContext = alfredgf::AFContext::new(&window, &context_config);
 }

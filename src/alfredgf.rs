6e3c72fae51ee43b76df96bedf14ec16b784a640
Lonsdaleiter <lonsdaleiteproductions@gmail.com>
2020-01-21 17:15:42 -0800

    doing thigns here

--- src/alfredgf.rs
+++ src/alfredgf.rs
@@ -43,50 +43,80 @@ pub struct AFWindow {
 
 }
 
-static mut CURRENT_WINDOW: Option<&AFWindow> = None;
+static mut CURRENT_WINDOW: Option<AFWindow> = None;
 
 impl AFWindow {
 
     fn new<'a>(config: &AFWindowConfig) -> &'a Self {
         unsafe {
-            let mut w: Option<&AFWindow> = None;
-            let k = match &CURRENT_WINDOW {
-                None => {
-                    let builder: WindowBuilder = WindowBuilder::new()
-                        .with_window_icon(Icon::from_rgba(
-                            config.icon.data.to_vec(),
-                            config.icon.width,
-                            config.icon.height,
-                        ).ok())
-                        .with_min_inner_size(
-                            PhysicalSize::new(config.min_size[0], config.min_size[1]))
-                        .with_max_inner_size(
-                            PhysicalSize::new(config.max_size[0], config.max_size[1]))
-                        .with_inner_size(
-                            PhysicalSize::new(config.start_size[0], config.start_size[1])
-                        )
-                        .with_title(config.title)
-                        .with_resizable(config.resizeable)
-                        .with_always_on_top(config.always_on_top)
-                        .with_maximized(config.maximized);
+            let builder: WindowBuilder = WindowBuilder::new()
+                .with_window_icon(Icon::from_rgba(
+                    config.icon.data.to_vec(),
+                    config.icon.width,
+                    config.icon.height,
+                ).ok())
+                .with_min_inner_size(
+                    PhysicalSize::new(config.min_size[0], config.min_size[1]))
+                .with_max_inner_size(
+                    PhysicalSize::new(config.max_size[0], config.max_size[1]))
+                .with_inner_size(
+                    PhysicalSize::new(config.start_size[0], config.start_size[1])
+                )
+                .with_title(config.title)
+                .with_resizable(config.resizeable)
+                .with_always_on_top(config.always_on_top)
+                .with_maximized(config.maximized);
 
-                    let event_loop: EventLoop<()> = EventLoop::new();
-                    let window = builder.build(&event_loop).unwrap();
-                    let aw: &AFWindow = &AFWindow {
-                        window,
-                        event_loop,
-                    };
-                    w = Option::Some(aw);
+            let event_loop: EventLoop<()> = EventLoop::new();
+            let window = builder.build(&event_loop).unwrap();
+            let t_w = Option::Some(AFWindow {
+                window,
+                event_loop,
+            });
 
-                    aw
-                }
-                Some(&AFWindow) => {
-                    CURRENT_WINDOW.unwrap()
-                }
-            };
+            CURRENT_WINDOW = t_w;
 
-            return k;
+            CURRENT_WINDOW.as_ref().unwrap()
         }
+//        unsafe {
+//            let mut w: Option<&AFWindow> = None;
+//            let k = match &CURRENT_WINDOW {
+//                None => {
+//                    let builder: WindowBuilder = WindowBuilder::new()
+//                        .with_window_icon(Icon::from_rgba(
+//                            config.icon.data.to_vec(),
+//                            config.icon.width,
+//                            config.icon.height,
+//                        ).ok())
+//                        .with_min_inner_size(
+//                            PhysicalSize::new(config.min_size[0], config.min_size[1]))
+//                        .with_max_inner_size(
+//                            PhysicalSize::new(config.max_size[0], config.max_size[1]))
+//                        .with_inner_size(
+//                            PhysicalSize::new(config.start_size[0], config.start_size[1])
+//                        )
+//                        .with_title(config.title)
+//                        .with_resizable(config.resizeable)
+//                        .with_always_on_top(config.always_on_top)
+//                        .with_maximized(config.maximized);
+//
+//                    let event_loop: EventLoop<()> = EventLoop::new();
+//                    let window = builder.build(&event_loop).unwrap();
+//                    let aw: &AFWindow = &AFWindow {
+//                        window,
+//                        event_loop,
+//                    };
+//                    w = Option::Some(aw);
+//
+//                    w.unwrap()
+//                }
+//                Some(&AFWindow) => {
+//                    w.unwrap()
+//                }
+//            };
+//
+//            return k;
+//        }
     }
 
 }
